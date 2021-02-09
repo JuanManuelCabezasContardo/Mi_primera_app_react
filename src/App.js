ab3e16e0cc9b2f562108a32ee9081feb3c8618af
JuanManuelCabezasContardo <juanmanuelcontardo@gmail.com>
2021-02-09 20:48:43 -0300

    primer_commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hola JS - bienvenido
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
